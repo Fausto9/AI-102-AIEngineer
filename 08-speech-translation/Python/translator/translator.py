e03c6df4f93d2598980905ec7a0dc1819814d82c
Fausto9 <73859734+Fausto9@users.noreply.github.com>
2023-05-07 19:38:19 -0600

    functions have been coded

--- 08-speech-translation/Python/translator/translator.py
+++ 08-speech-translation/Python/translator/translator.py
@@ -1,8 +1,13 @@
+pip install azure-cognitiveservices-speech==1.24.0
+pip install playsound==1.2.2
+
 from dotenv import load_dotenv
 from datetime import datetime
 import os
+from playsound import playsound
 
 # Import namespaces
+import azure.cognitiveservices.speech as speech_sdk
 
 
 def main():
@@ -16,9 +21,16 @@ def main():
         cog_region = os.getenv('COG_SERVICE_REGION')
 
         # Configure translation
+        translation_config = speech_sdk.translation.SpeechTranslationConfig(cog_key, cog_region)
+        translation_config.speech_recognition_language = 'en-US'
+        translation_config.add_target_language('fr')
+        translation_config.add_target_language('es')
+        translation_config.add_target_language('hi')
+        print('Ready to translate from',translation_config.speech_recognition_language)
 
 
         # Configure speech
+        speech_config = speech_sdk.SpeechConfig(cog_key, cog_region)
 
 
         # Get user input
@@ -38,6 +50,24 @@ def Translate(targetLanguage):
     translation = ''
 
     # Translate speech
+    """audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
+    translator = speech_sdk.translation.TranslationRecognizer(translation_config, audio_config = audio_config)
+    print("Speak now...")
+    result = translator.recognize_once_async().get()
+    print('Translating "{}"'.format(result.text))
+    translation = result.translations[targetLanguage]
+    print(translation)"""
+
+    # Translate speech
+    audioFile = 'station.wav'
+    playsound(audioFile)
+    audio_config = speech_sdk.AudioConfig(filename=audioFile)
+    translator = speech_sdk.translation.TranslationRecognizer(translation_config, audio_config = audio_config)
+    print("Getting speech from file...")
+    result = translator.recognize_once_async().get()
+    print('Translating "{}"'.format(result.text))
+    translation = result.translations[targetLanguage]
+    print(translation)
 
 
     # Synthesize translation
